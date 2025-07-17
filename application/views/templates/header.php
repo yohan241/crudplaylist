3dc39f3b76283e40b8638231abf06faf4b8f4856
yohan241 <johanczarpagba@gmail.com>
2025-07-17 08:50:15 +0800

    Initial CI Setup

---
+++ application/views/templates/header.php
@@ -0,0 +1,20 @@
+
+    <!DOCTYPE html>
+    <html lang="en" data-bs-theme="auto">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+
+        <title>CrudPlaylist</title>
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
+        <script src="/docs/5.3/assets/js/color-modes.js"></script>
+        <link href="/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr">
+        <link href="https://bootswatch.com/5/brite/bootstrap.min.css" rel="stylesheet">
+       
+            
+    </head>
+
+    <body>
